26cee2f94bfd1017a18382d11c08b766bf9b88c3
emimontanari <emimontanari@icloud.com>
2022-04-27 16:56:27 -0400

    Agregando eventos

--- js/app.js
+++ js/app.js
@@ -1,62 +1,81 @@
 // sacar el promedio de nota final y decir si esta aprobado o no
-let nombreAlumno = prompt('Ingrese el nombre del alumno');
-let primeraNota = parseInt(prompt('Ingrese la primera nota del alumno'));
-let segundaNota = parseInt(prompt('Ingrese la segunda nota del alumno'));
-let terceraNota = parseInt(prompt('Ingrese la tercera nota del alumno'));
+// let nombreAlumno = prompt('Ingrese el nombre del alumno');
+// let primeraNota = parseInt(prompt('Ingrese la primera nota del alumno'));
+// let segundaNota = parseInt(prompt('Ingrese la segunda nota del alumno'));
+// let terceraNota = parseInt(prompt('Ingrese la tercera nota del alumno'));
 
-const notas = [primeraNota, segundaNota,terceraNota];
-const promedio = notas.length
-//Sumar todas las notas mediante reduce
-let notasTotal = notas.reduce(function(acumulador, valorActual){
-    return acumulador + valorActual;
-});
+// const notas = [primeraNota, segundaNota,terceraNota];
+// const promedio = notas.length
+// Sumar todas las notas mediante reduce
+// let notasTotal = notas.reduce(function(acumulador, valorActual){
+//     return acumulador + valorActual;
+// });
 
 // sacar promedio de notasTotal dividido entre 3
-function  promedioNotas(notasTotal){
-    return Math.round(notasTotal / promedio);
-};
-let resultado = promedioNotas(notasTotal);
+// function  promedioNotas(notasTotal){
+//     return Math.round(notasTotal / promedio);
+// };
+// let resultado = promedioNotas(notasTotal);
 
 
-alert(`El promedio de ${nombreAlumno} es ${resultado}`);// IMPRIMIR RESULTADO
-
-let aprobado = resultado >= 6 ? 'APROBADO' : 'DESAPROBADO';
-
-alert(`El estado de ${nombreAlumno} es ${aprobado}`);// IMPRIMIR EL ESTADO DEL ALUMNO
+// alert(`El promedio de ${nombreAlumno} es ${resultado}`);// IMPRIMIR RESULTADO
 
 
-//CREAR UN OBJETO CON LOS DATOS DEL ALUMNO Y IMPIMPRIMERLO
-class Alumno {
-    constructor(nombre,promedio,estado){
-        this.nombre = nombre;
-        this.promedio = promedio;
-        this.estado = estado;
-    }
-};
+// alert(`El estado de ${nombreAlumno} es ${aprobado}`);// IMPRIMIR EL ESTADO DEL ALUMNO
 
-const alumno = new Alumno(nombreAlumno,resultado,aprobado);
+
+
+
+
 window.addEventListener('load', imprimirResultado);
-
-
+// Variables globales
 const formulario = document.querySelector('#formulario');
+// eventos
+formulario.addEventListener('submit', enviarFormulario);
+//funciones
+function enviarFormulario(e){
+  e.preventDefault();
+  let nombre = document.querySelector('#nombre').value;
+  let primeraNota = parseInt(document.querySelector('#nota1').value)
+  let segundaNota = parseInt(document.querySelector('#nota2').value)
+  let terceraNota = parseInt(document.querySelector('#nota3').value)
+  const notas = [primeraNota, segundaNota,terceraNota];
+  const promedio = notas.length
+  let notasTotal = notas.reduce(function(acumulador, valorActual){
+    return acumulador + valorActual;
+  });
+  function  promedioNotas(notasTotal){
+    return Math.round(notasTotal / promedio);
+  }
+  const resultado = promedioNotas(notasTotal);
+  const estado = resultado >= 6 ? 'APROBADO' : 'DESAPROBADO';
 
+  imprimirResultado(nombre,resultado,estado);
+  // ocultar los resultados hasta que se envie el formulario
+  const contenedorResultado = document.querySelector('#resultado');
+  contenedorResultado.classList.remove('oculto');
+  
+};
 
-function imprimirResultado(){
+function imprimirResultado(nombre,promedio,estado){
+  // vaciar el contenido del div
+  const contenedorResultado = document.querySelector('#resultado');
+  contenedorResultado.innerHTML = '';
+
   let div = document.createElement('div');
-    div.innerHTML = `
-        <p class="header"> Resultado </p>
-        <p class="font-bold"> Nombre: <span class="font-normal">${alumno.nombre} </span> </p>
-        <p class="font-bold"> Promedio: <span class="font-normal">${alumno.promedio} </span> </p>
-        <p class="font-bold"> Estado: <span id="status" class="font-normal">${alumno.estado} </span> </p>
+  div.innerHTML = `
+  <p class="header"> Resultado </p>
+  <p class="font-bold"> Nombre: <span class="font-normal">${nombre} </span> </p>
+  <p class="font-bold"> Promedio: <span class="font-normal">${promedio} </span> </p>
+        <p class="font-bold"> Estado: <span id="status" class="font-normal">${estado} </span> </p>
     `;
     const resultadoDiv = document.querySelector('#resultado');
     resultadoDiv.appendChild(div);
 
     let h3 = document.querySelector('#status');
-    if(alumno.estado === 'APROBADO'){
+    if(estado === 'APROBADO'){
       h3.classList.add('verde');
       }else{
         h3.classList.add('rojo');
       }
 };
-
